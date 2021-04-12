185e718f27a6496fb239ec3ed9dd14eb60ec3894
VladZAH <54724049+VladZAH@users.noreply.github.com>
2021-04-12 14:23:20 -0700

    Update combinationSum.js

--- combinationSum.js
+++ combinationSum.js
@@ -1,1 +1,14 @@
 
+var combinationSum4 = function(nums, target) {
+    let M = new Map();
+    let ways = 0;
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] <= target){
+            M[nums[i]] = Math.floor(target / nums[i]);
+        }
+    }
+    
+    return M
+};
+
+combinationSum4([1,2,3,4,5,6], 4)
